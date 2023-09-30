8868cf397b95b8b56725986be37683f217f38df4
Bianca Jessa A. Carabio <89141786+polyxenna@users.noreply.github.com>
2023-09-30 15:52:20 +0800

    cookbook update

--- cookbook.py
+++ cookbook.py
@@ -2,7 +2,10 @@ import streamlit as st
 
 def run():
     st.title('Cookbook')
-    st.write('This is the cookbook page.')
     if st.button("Return to Dashboard"):
         st.session_state.runpage = 'dashboard'
         st.experimental_rerun()
+    st.write('## What is your data set?')
+    st.write('Our data set is the evolution of Intel CPUs')
+    st.write('## Where did you acquire your data set?')
+    st.write('We acquired it online via kaggle')
